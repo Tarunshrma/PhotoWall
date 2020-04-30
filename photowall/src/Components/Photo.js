c66dda4c873e3b258ddbca6bd91dec8cb33d3b6d
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-04-30 16:01:50 +0530

    Added photos to photo grid

--- photowall/src/Components/Photo.js
+++ photowall/src/Components/Photo.js
@@ -3,8 +3,14 @@ import "../styles/stylesheet.css"
 
 class Photo extends Component{
     render(){
-        const photo = this.props.photo;
-        return <figure className="figure"></figure>
+        const photo = this.props.post;
+        return <figure className="figure">
+            <img className="photo" src={photo.imageLink} alt={photo.description}></img>
+            <figcaption><p>{photo.description}</p></figcaption>
+            <div className="button-container">
+                <button className="button">Remove</button>
+            </div>
+        </figure>
     }
 }
 
