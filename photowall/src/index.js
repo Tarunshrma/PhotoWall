09473eabfef42e4a47637d47133b82ba02b821d3
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-01 14:33:47 +0530

    Added rerdux dev tool support

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -6,6 +6,8 @@ import rootReducer from './redux/Reducer';
 import {Provider} from 'react-redux'
 import App from "./Components/App"
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
 
 ReactDom.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>,document.getElementById("root"));
