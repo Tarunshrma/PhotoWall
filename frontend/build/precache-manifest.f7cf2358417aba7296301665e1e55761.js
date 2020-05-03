431161829a6755194654e11a5c9681329e3cd715
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-03 11:07:31 +0530

    Change in project structure

---
+++ frontend/build/precache-manifest.f7cf2358417aba7296301665e1e55761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc8079f9b7a7fcd336da5851b884124",
+    "url": "/learning-reactjs/index.html"
+  },
+  {
+    "revision": "c8219ddce1ed8054d1cd",
+    "url": "/learning-reactjs/static/css/main.6f9ed616.chunk.css"
+  },
+  {
+    "revision": "4743968726d67213eb2c",
+    "url": "/learning-reactjs/static/js/2.5ea941eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learning-reactjs/static/js/2.5ea941eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8219ddce1ed8054d1cd",
+    "url": "/learning-reactjs/static/js/main.9a27d8f5.chunk.js"
+  },
+  {
+    "revision": "a7f7b0b1f397e21b60a3",
+    "url": "/learning-reactjs/static/js/runtime-main.1ae6dbbd.js"
+  }
+]);
