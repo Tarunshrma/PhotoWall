3f3327e175a7aa0d18de53a680f4f30ca9d1cfc2
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-07 09:46:16 +0530

    change create post api to enable image upload

--- serverless-backend/src/lambda/http/addPost.ts
+++ serverless-backend/src/lambda/http/addPost.ts
@@ -4,17 +4,26 @@ import * as AWS  from 'aws-sdk'
 import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
 
 const postsTable = process.env.POSTS_TABLE
+const bucketName = process.env.POST_IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
 
   const parsedBody  = JSON.parse(event.body);
   const postId = uuid.v4();
+  const imageId = uuid.v4();
+
+  const url = getUploadUrl(imageId)
 
   const newPost = {
       postId: postId,
+      ImageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`,
       ...parsedBody
   }
 
@@ -29,7 +38,16 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-        newPost
+        newPost: newPost,
+        uploadUrl: url
     })
   }
+}
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: 300
+  })
 }
