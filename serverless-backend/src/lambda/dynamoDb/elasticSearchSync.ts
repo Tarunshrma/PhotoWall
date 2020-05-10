6e036c6675bce3dc92738337ae0e9f45fd0bd10f
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-10 13:20:51 +0530

    Integrated elastic search

--- serverless-backend/src/lambda/dynamoDb/elasticSearchSync.ts
+++ serverless-backend/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -1,10 +1,50 @@
 import { DynamoDBStreamHandler, DynamoDBStreamEvent } from 'aws-lambda'
 import 'source-map-support/register'
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
 
+const esHost = process.env.ES_ENDPOINT
+
+const es = new elasticsearch.Client({
+    hosts: [ esHost ],
+    connectionClass: httpAwsEs
+  })
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
 
-    for(const record of event.Records){
-        console.log('Processing Records: ',JSON.stringify(record))
+    //console.log('Processing events batch from DynamoDB', JSON.stringify(event))
+
+    for (const record of event.Records) {
+      console.log('Processing record', JSON.stringify(record))
+      if (record.eventName !== 'INSERT') {
+        continue
+      }
+  
+      const newItem = record.dynamodb.NewImage
+
+      console.log('New Item', JSON.stringify(newItem))
+  
+      const postId = newItem.postId.S
+
+      console.log('Post Id: ', postId)
+      console.log('Image Url: ', newItem.ImageUrl.S)
+      console.log('Descriptioin: ', newItem.description.S)
+  
+      const body = {
+        postId: newItem.postId.S,
+        imageUrl: newItem.ImageUrl.S,
+        description: newItem.description.S
+      }
+
+      console.log('Body to bee inserteed in ESs', JSON.stringify(body))
+  
+      await es.index({
+        index: 'post-index',
+        type: 'images',
+        id: postId,
+        body
+      })
+  
     }
+    
 }
