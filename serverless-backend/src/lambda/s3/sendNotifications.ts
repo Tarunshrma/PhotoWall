79e206672221f8b7f153b004f5fd66ec68b236a7
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-10 21:01:40 +0530

    Replaaced lambda notificaction with SNS notification

--- serverless-backend/src/lambda/s3/sendNotifications.ts
+++ serverless-backend/src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { S3Event, S3Handler } from 'aws-lambda'
+import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 
@@ -16,26 +16,36 @@ const connectionParams = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
 
-export const handler: S3Handler = async (event: S3Event) => {
+export const handler: SNSHandler = async (event: SNSEvent) => {
     
-    for(const post of event.Records){
-        const key = post.s3.object.key;
-        console.log("Processing S3 item with the key :", key);
+    for(const snsRecord of event.Records)
+    {
+        const s3EventStr = snsRecord.Sns.Message
+        console.log('Processing S3 event', s3EventStr)
+        const s3Event = JSON.parse(s3EventStr)
 
-        const connections = await docClient.scan({
-            TableName: connectionsTable
-        }).promise()
-    
-        const payload = {
-            imageId: key
-        }
-    
-        for (const connection of connections.Items) {
-            const connectionId = connection.id
-            await sendMessageToClient(connectionId, payload)
-        }
+        await processS3Event(s3Event)
+    }   
+  }
+
+  async function processS3Event(s3Event: S3Event) {
+    for (const record of s3Event.Records) {
+      const key = record.s3.object.key
+      console.log('Processing S3 item with key: ', key)
+  
+      const connections = await docClient.scan({
+          TableName: connectionsTable
+      }).promise()
+  
+      const payload = {
+          imageId: key
+      }
+  
+      for (const connection of connections.Items) {
+          const connectionId = connection.id
+          await sendMessageToClient(connectionId, payload)
+      }
     }
-   
   }
 
   async function sendMessageToClient(connectionId, payload) {
